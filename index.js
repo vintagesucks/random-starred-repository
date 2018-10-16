c3c15bc0695f86cda3196e59fa3cf4b04beb2dfb
Nikolas Evers <vintagesucks@users.noreply.github.com>
2018-10-16 19:17:52 +0200

    pick random entry; clearer function names (#5)
    * pick random entry, not first
    
    * tidy
    
    * rename functions

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 const got = require("got");
 
 let user = process.argv.slice(2)[0];
+let randomEntry = Math.floor(Math.random() * 29) + 0;
+let lastPage = new RegExp(/(.*)page=(.*)>; rel=\"last\"/);
 
 if (!user) {
   console.log("Usage: `node index.js username`");
   process.exit(1);
 }
 
-const stars = (user, page) =>
+const getStars = (user, page) =>
   got(`https://api.github.com/users/${user}/starred?page=${page}`)
     .then((res) => JSON.parse(res.body))
     .then((starred) =>
@@ -19,9 +21,7 @@ const stars = (user, page) =>
       }))
     );
 
-let lastPage = new RegExp(/(.*)page=(.*)>; rel=\"last\"/);
-
-const randomPage = (user) =>
+const getRandomPage = (user) =>
   got(`https://api.github.com/users/${user}/starred`)
     .then((res) =>
       res.headers.link
@@ -29,8 +29,8 @@ const randomPage = (user) =>
     )
     .then((pages) => Math.floor(Math.random() * pages) + 1);
 
-randomPage(user)
-  .then((page) => stars(user, page))
+getRandomPage(user, randomEntry)
+  .then((page) => getStars(user, page))
   .then((result) =>
-    console.log("https://github.com/" + result[0].owner + "/" + result[0].repo)
+    console.log("https://github.com/" + result[randomEntry].owner + "/" + result[randomEntry].repo)
   );
