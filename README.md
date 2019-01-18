33b559c67a372a70b6eb4796cb951da9744bfde4
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-01-18 04:52:46 +0100

    xo (#18)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # random-starred-repository
 
-[![Travis Badge](https://api.travis-ci.org/vintagesucks/random-starred-repository.svg?branch=master)](https://travis-ci.org/vintagesucks/random-starred-repository) [![Dependabot Badge](https://img.shields.io/badge/Dependabot-enabled-blue.svg)](https://dependabot.com/) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/19f44650e8ef48d68e3ec6837bead128)](https://app.codacy.com/app/vintagesucks/random-starred-repository)
+[![Travis Badge](https://api.travis-ci.org/vintagesucks/random-starred-repository.svg?branch=master)](https://travis-ci.org/vintagesucks/random-starred-repository) [![Dependabot Badge](https://img.shields.io/badge/Dependabot-enabled-blue.svg)](https://dependabot.com/) [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/xojs/xo)
 
 Returns a random repository starred by a user
 
--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-"use strict";
+#!/usr/bin/env node
 
-const got = require("got");
-const { green, red } = require('kleur');
+'use strict';
 
-let user = process.argv.slice(2)[0];
-let randomEntry = Math.floor(Math.random() * 30);
-let lastPage = new RegExp(/(.*)page=(.*)>; rel=\"last\"/);
+const got = require('got');
+const {green, red} = require('kleur');
+
+const user = process.argv.slice(2)[0];
+const randomEntry = Math.floor(Math.random() * 30);
+const lastPage = new RegExp(/(.*)page=(.*)>; rel="last"/);
 
 if (!user) {
-  console.log("Usage: `node index.js username`");
+  console.log('Usage: `node index.js username`');
   process.exit(1);
 }
 
 const getStars = (user, page) =>
   got(`https://api.github.com/users/${user}/starred?page=${page}`)
-    .then((res) => JSON.parse(res.body))
-    .then((starred) =>
-      starred.map((s) => ({
+    .then(res => JSON.parse(res.body))
+    .then(starred =>
+      starred.map(s => ({
         owner: s.owner.login,
         repo: s.name
       }))
     )
-    .catch((error) => {
-      console.error(red().bold('Unable to get stars (' + error.statusCode + ' ' + error.statusMessage + ')'))
-      process.exit(0)
+    .catch(error => {
+      console.error(red().bold('Unable to get stars (' + error.statusCode + ' ' + error.statusMessage + ')'));
+      process.exit(0);
     });
 
-const getRandomPage = (user) =>
+const getRandomPage = user =>
   got(`https://api.github.com/users/${user}/starred`)
-    .then((res) =>
+    .then(res =>
       res.headers.link
-        .replace(lastPage, "$2")
+        .replace(lastPage, '$2')
     )
-    .then((pages) => Math.floor(Math.random() * pages) + 1)
-    .catch((error) => console.error(red().bold('Unable to get random page, falling back to first page (' + error.statusCode + ' ' + error.statusMessage + ')')));
+    .then(pages => Math.floor(Math.random() * pages) + 1)
+    .catch(error => console.error(red().bold('Unable to get random page, falling back to first page (' + error.statusCode + ' ' + error.statusMessage + ')')));
 
 getRandomPage(user)
-  .then((page) => getStars(user, page))
-  .then((result) =>
+  .then(page => getStars(user, page))
+  .then(result =>
     console.log(green().bold(
-      "https://github.com/" + result[randomEntry].owner + "/" + result[randomEntry].repo
+      'https://github.com/' + result[randomEntry].owner + '/' + result[randomEntry].repo
     ))
   );
